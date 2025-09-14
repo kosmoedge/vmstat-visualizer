efeeedd02a902494609f2e5668915df3459702ab
nikfot <nik_fot@hotmail.gr>
2025-09-14 22:48:07 +0300

    Add cli format

---
+++ src/vmstat-visualizer/cli/__main__.py
@@ -0,0 +1,25 @@
+"""
+This is the entrypoint to netcdfella app.
+"""
+import click
+
+from netcdfella.cli import converter, server, watcher
+
+
+@click.group("vmstat-visualizer", no_args_is_help=True)
+def cli():
+    """vmstat-visualizer is the cli for vmstat-visualizer.
+    Use it to visualize vmstat logs.
+    """
+
+
+def main():
+    "main is the entrypoint of vmstat-visualizer"
+    cli.add_command(watcher.watch)
+    cli.add_command(server.serve)
+    cli.add_command(converter.convert)
+    cli(prog_name="vmstat-visualizer")
+
+
+if __name__ == "__main__":
+    main()
