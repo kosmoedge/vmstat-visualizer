276d2f17441ec325999e602c3cb21ae4a2ae3bc9
nikfot <nik_fot@hotmail.gr>
2025-10-05 17:04:58 +0300

    Add flag option names and defaults
    Signed-off-by: nikfot <nik_fot@hotmail.gr>

--- src/vmstat_visualizer/cli/visualizer.py
+++ src/vmstat_visualizer/cli/visualizer.py
@@ -6,22 +6,22 @@ import click
 from vmstat_visualizer.parser.parser import Parser
 
 @click.command("visualize", no_args_is_help=True)
-@click.argument("file", default="./vmstat.log", type=click.Path(exists=True))
+@click.argument("file", default="vmstat.log", type=click.Path(exists=True))
 @click.option(
     "-o",
-    "--output-dir",
-    default="./",
-    help="set the output directory for converted files.",
+    "--output-prefix",
+    default="vmstat",
+    help="set the output prefix for created files.",
 )
 @click.option(
-    "-k",
-    "--output-kinds",
+    "-e",
+    "--output-extension",
     default="png",
-    help="set the output kind for conversion.",
+    help="set the output extension for created files.",
 )
 
 def visualize(
-    file, output_dir, output_kinds
+    file, output_prefix, output_extension
 ):
     """
     Visualize a file or files in a directory
@@ -30,5 +30,8 @@ def visualize(
     parser = Parser(file)
     parser.parse()
     print(f">>> Parsed {len(parser.timeseries)} time series entries.")
-
+    parser.plot(
+        output_file_prefix=f"{output_prefix}",
+        output_format=output_extension
+    )
     # Here you would add code to generate visualizations from parser.timeseries
